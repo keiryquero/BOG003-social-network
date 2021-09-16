73b6f1f4c274f1a9a7ff6c563a96f0b2e83a549f
Marieth Verónica Bustos <veronica_9431@outlook.com>
2021-09-16 08:35:01 -0500

    cambios en el login.js

--- src/login.js
+++ src/login.js
@@ -1,21 +1,23 @@
 export const login = () => {
-
-  const templateLogin = `
+    const templateLogin = `
       <div> 
-       <h1> Logueate:</h1>,
-      <p>Inicie sesión</p>
+      <div> <img class="galeria"src="img/ejemplorestaurantes.jpg" alt="Imagen ejemplo restaurantes"><br><br>
+      </div> 
+      <div> 
+       <h1> welcome back </h1>,
+       </div> 
+      // <p>Inicie sesión</p>//
       <input type="email" class="form" id="email-registro" value="" placeholder="Email">
       <input type= "password" class="form" id="password-one" value="" placeholder="Password">
-      <button id="btn-login" type="button">Logueate</button>
+      <button id="btn-login" type="button">Ingresar</button>
       <p>¿NO estas registrado aun? <a href="#">ingresa aquí</a></p>
       <button id="botonRetornoRegistro" type="button">Registrarse</button>
       </div>
-  `
-  /*const divRegistro = document.createElement("div");
+  `;
+    /*const divRegistro = document.createElement("div");
   divRegistro.innerHTML = templateRegistro;
 
   divRegistro.classList.add("divRegistro") /*Se asigna la clase a un div creado desde createElement*/
-  //console.log()
-  return templateLogin
-
-}
+    //console.log()
+    return templateLogin;
+};
