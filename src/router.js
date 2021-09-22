ff069f5ed63e2a99ac6070ab5e373b650fb2a325
Marieth Verónica Bustos <veronica_9431@outlook.com>
2021-09-22 11:34:00 -0500

    vistas completas en registro y login.

--- src/router.js
+++ src/router.js
@@ -4,30 +4,35 @@ import { login } from "./login.js";
 import { muro } from "./muro.js";
 
 export function mostrarVista(hash) {
-  const container = document.getElementById("contenedorRood");
+    const container = document.getElementById("contenedorRoute");
 
-  switch (hash) {
-    case "#/inicio":
-      container.innerHTML = registration();
-      break;
+    //limpia el contenedor para cada vez que s evaya mostrar una vista
+    container.innerHTML = "";
 
-    case "#/login":
-      container.innerHTML = login();
-      //container.appendChild(templateLogin());
-      // container.innerHTML = "<h1>Login</h1><a href='#/inicio'> Inicio</a>"
-      break;
-    case "#/post":
-      container.innerHTML = post();
-      // container.innerHTML = "<h1>Login</h1><a href='#/inicio'> Inicio</a>"
-      break;
-    case "#/muro":
-      container.innerHTML = muro();
-      // container.innerHTML = "<h1>Login</h1><a href='#/inicio'> Inicio</a>"
-      break;
-    default:
-      container.innerHTML = registration();
-      break;
-  }
+    // se agrega appenchild porque el metodo ya no devuelve el templete
+    // como texto sino que lo devuelve como nodo de HTML
+    switch (hash) {
+        case "#/inicio":
+            container.appendChild(registration());
+            break;
+
+        case "#/login":
+            container.appendChild(login());
+            //container.appendChild(templateLogin());
+            // container.innerHTML = "<h1>Login</h1><a href='#/inicio'> Inicio</a>"
+            break;
+        case "#/post":
+            container.innerHTML = post();
+            // container.innerHTML = "<h1>Login</h1><a href='#/inicio'> Inicio</a>"
+            break;
+        case "#/muro":
+            container.innerHTML = muro();
+            // container.innerHTML = "<h1>Login</h1><a href='#/inicio'> Inicio</a>"
+            break;
+        default:
+            container.innerHTML = registration();
+            break;
+    }
 }
 
 /*ejemplo 
