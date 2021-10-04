f8768f8f151ec64c75ec2877a7bd7b9478b6d88a
keiry <keiryquero@gmail.com>
2021-10-04 13:36:53 -0500

    sin cambios relevantes

--- src/post.js
+++ src/post.js
@@ -1,9 +1,3 @@
-/*import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
-import {
-  getFirestore,
-  doc,
-  setDoc,
-} from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";*/
 import {
   collection,
   addDoc,
@@ -11,24 +5,11 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 //import { app } from "./main.js";
 import db from "./main.js";
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDGmKzcSHyRp56lPmYE9r4hCsHnT0jeR_Y",
-  authDomain: "social-network-1c5d2.firebaseapp.com",
-  projectId: "social-network-1c5d2",
-  storageBucket: "social-network-1c5d2.appspot.com",
-  messagingSenderId: "514107260716",
-  appId: "1:514107260716:web:77ff03537478a07489b689",
-  measurementId: "G-3GHVJ77MJ3",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);*/
 
 export const post = () => {
     //se crea un elemento tipo template para insertarle el texto de la plantilla
   let contenedor = document.createElement("div");
   //let contenedorTarjeta = document.createElement("div");
-
 
   const templatePost = `
     <section id ="contenedor-post" class = "contenedor-post">
@@ -60,7 +41,7 @@ export const post = () => {
 
   // se obtiene el formulario dentro el elemnto template
   let formPost = contenedor.querySelector("#formulario-post");
-  let tarjetas = contenedorTarjeta.querySelector("#contenedor-Publicacion");
+  //let tarjetas = contenedorTarjeta.querySelector("#contenedor-Publicacion");
 
   formPost.addEventListener("submit", (evt) => {
     evt.preventDefault();
