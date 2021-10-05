4277c0e5b6b74ca1dab2844896ac253f8c9bc180
Marieth Verónica Bustos <veronica_9431@outlook.com>
2021-10-05 08:20:14 -0500

    cambios post

--- src/post.js
+++ src/post.js
@@ -11,18 +11,8 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 //import { app } from "./main.js";
 import db from "./main.js";
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDGmKzcSHyRp56lPmYE9r4hCsHnT0jeR_Y",
-  authDomain: "social-network-1c5d2.firebaseapp.com",
-  projectId: "social-network-1c5d2",
-  storageBucket: "social-network-1c5d2.appspot.com",
-  messagingSenderId: "514107260716",
-  appId: "1:514107260716:web:77ff03537478a07489b689",
-  measurementId: "G-3GHVJ77MJ3",
-};
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);*/
+
 
 export const post = () => {
     //se crea un elemento tipo template para insertarle el texto de la plantilla
@@ -60,7 +50,7 @@ export const post = () => {
 
   // se obtiene el formulario dentro el elemnto template
   let formPost = contenedor.querySelector("#formulario-post");
-  let tarjetas = contenedorTarjeta.querySelector("#contenedor-Publicacion");
+  /*let tarjetas = contenedorTarjeta.querySelector("#contenedor-Publicacion");*/
 
   formPost.addEventListener("submit", (evt) => {
     evt.preventDefault();
